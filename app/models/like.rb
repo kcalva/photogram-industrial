e94bb11dc395ee67b7e82cc363af0a45ff314c32
kcalva <kcalva737@gmail.com>
2024-07-02 13:32:41 -0500

    added scope to uniqueness to filter only for a given photo. Got rid of presence

--- app/models/like.rb
+++ app/models/like.rb
@@ -22,5 +22,5 @@ class Like < ApplicationRecord
   belongs_to :fan, class_name: "User", counter_cache: true
   belongs_to :photo, counter_cache: true
 
-  validates :fan_id, presence: true, uniqueness: true
+  validates :fan_id, uniqueness: { scope: :photo_id, message: "photo was already liked" }
 end
